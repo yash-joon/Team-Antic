3bcecbd138db02a490c85e8dcbfcf871ad9da5bd
michael.yun.antra <michael.yun.antra@gmail.com>
2025-02-20 18:17:52 -0500

    Implemented settings into nav

--- BankingProject/src/app/app-routing.module.ts
+++ BankingProject/src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HomePageComponent } from './home-page/home-page.component';
 import { RewardsPageComponent } from './rewardsPage/rewards-page.component';
+import { SettingsComponent } from './settings/settings.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
   { path: 'home', component: HomePageComponent },
   // TODO: Connect Settings page
-  // { path: 'settings', ... },
-  { path: 'rewards', component: RewardsPageComponent},
+  { path: 'settings', component: SettingsComponent },
+  { path: 'rewards', component: RewardsPageComponent },
   // TODO: Connect Store Locator page
   // { path: 'locator', ...},
   { path: '**', redirectTo: '/home' } // Redirect unknown routes to home
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- BankingProject/src/app/app.module.ts
+++ BankingProject/src/app/app.module.ts
@@ -8,19 +8,25 @@ import { RewardsPageModule } from './rewardsPage/rewards-page.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RewardsPageComponent } from './rewardsPage/rewards-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { SettingsComponent } from './settings/settings.component';
+import { SecurityComponent } from './settings/security/security.component';
+import { PersonalDetailsComponent } from './settings/personal-details/personal-details.component';
+import { SignInPreferencesComponent } from './settings/sign-in-preferences/sign-in-preferences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    NavbarComponent
-
+    NavbarComponent,
+    SettingsComponent,
+    SecurityComponent,
+    PersonalDetailsComponent,
+    SignInPreferencesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RewardsPageModule,
+    RewardsPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
